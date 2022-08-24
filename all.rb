97caec16e05c7def4dc6d2da7abdf5fd0043c82e
akmhmgc <akimuhamaguchi@icloud.com>
2022-08-24 19:27:34 +0900

    委譲記法

--- all.rb
+++ all.rb
@@ -1,10 +1,17 @@
-# 任意の引数を受け取ることができるメソッド
+任意の引数を受け取ることができるメソッド
 def foo(*array, **hash, &block)
   p array #=> ["a", "b"]
   p hash #=> {:c=>"x", :d=>"y"}
   p block.call #=> 1
 end
 
-foo("a", "b", c: "x", d: "y") do
+
+#　委譲記法
+def bar(arg1, ...)
+  p "arg1: #{arg1}"
+  p(...)
+end
+
+bar("a", "b", c: "x", d: "y") do
   1
 end
